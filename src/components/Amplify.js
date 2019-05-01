de5328b491bfeed4a9de354fc21ae462cc94b6a1
leomartinez2019 <lowenhard@yahoo.com>
2019-05-01 04:10:57 -0500

    initial commit

---
+++ src/components/Amplify.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Amplify = () => (
+  <div>
+    <h1>Deploy with Amplify</h1>
+    <p>Depolying an app was never so easy with AWS</p>
+  </div>
+)
+
+export default Amplify;
